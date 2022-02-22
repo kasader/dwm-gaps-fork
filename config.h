36aaae403b0250f374694fd49b8c52a8a7d0df92
Kasadasan <casada980@gmail.com>
2022-02-21 21:24:03 -0500

    Fixed keybind error.

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_ichigo2, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "termite", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *termcmd2[] = { "st", NULL };
 
 static Key keys[] = {
